472286ceb9a9251ff26ea4240b4d80944fc66c3d
appswebdev <appswebdev@gmail.com>
2016-09-20 19:34:02 +0300

    sign in with google

--- app/src/main/java/college/minhal/fire/activities/LoginActivity.java
+++ app/src/main/java/college/minhal/fire/activities/LoginActivity.java
@@ -8,11 +8,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.FirebaseDatabase;
 
 import college.minhal.fire.R;
@@ -20,8 +29,11 @@ import college.minhal.fire.models.User;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_GOOGLE = 12;
     EditText etEmail;
     EditText etPassword;
+    private GoogleApiClient mGoogleApiClient;
+    private View btnGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +42,64 @@ public class LoginActivity extends AppCompatActivity {
 
         etEmail = (EditText) findViewById(R.id.etEmail);
         etPassword = (EditText) findViewById(R.id.etPassword);
+
+        btnGoogle = findViewById(R.id.btnGoogleLogin);
+        assert btnGoogle != null;
+
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInWithGoogle();
+            }
+        });
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .enableAutoManage(this, null)
+                .build();
+    }
+
+    private void signInWithGoogle() {
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(intent, REQUEST_CODE_GOOGLE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_GOOGLE && resultCode == RESULT_OK) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            GoogleSignInAccount account = result.getSignInAccount();
+
+            //
+            AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+
+            FirebaseAuth.getInstance().signInWithCredential(credential).
+                    addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    FirebaseUser user = authResult.getUser();
+                    User u = new User(user.getDisplayName(), user.getEmail());
+
+                    FirebaseDatabase.getInstance().getReference().child("Users")
+                            .child(user.getUid()).setValue(u);
+                    gotoMain();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    showError(e, btnGoogle);
+                }
+            });
+
+
+        }
     }
 
     public void login(final View view) {
@@ -65,7 +135,7 @@ public class LoginActivity extends AppCompatActivity {
             isValid = false;
         }
 
-        if (getPassword() == null ||getPassword().length() < 6) {
+        if (getPassword() == null || getPassword().length() < 6) {
             etPassword.setError("Password must contain at least 6 characters");
             isValid = false;
         }
@@ -115,7 +185,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
-    private void showError(Exception e, View view ) {
+    private void showError(Exception e, View view) {
         hideProgress();
         Snackbar.make(view,
                 e.getLocalizedMessage(),
